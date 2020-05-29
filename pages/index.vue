0b7cd3a3a85479bbdd00d6c49ea2410dc2ee649b
David Parks <davidthomasparks@gmail.com>
2020-05-29 11:42:46 -0500

    increase logo size desktop

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center min-h-screen">
-    <img class="w-auto h-24 object-contain" src="~/assets/logos/duke-basic.png" />
+    <img class="w-auto h-24 sm:h-48 object-contain" src="~/assets/logos/duke-basic.png" />
   </div>
 </template>
 
