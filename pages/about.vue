8df08eb6a979879a89e3c6b97aa161f445aeb131
David Parks <Davidthomasparks@gmail.com>
2022-09-22 15:22:05 -0700

    update about me

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center justify-center min-h-screen flex-col">
     <p>DUKE TALENT IS A LOS ANGELES-BASED SEARCH AND CONSULTING AGENCY SPECIALIZING IN HIGH-END DESIGN, ARCHITECTURE, AND REAL ESTATE DEVELOPMENT.</p>
     <p class="font-bold uppercase text-left max-w-sm w-full text-xs mt-12">Contact</p>
-    <p class="max-w-sm uppercase text-xs">24 Park Ave</p>
+    <p class="max-w-sm uppercase text-xs">Bucket Shop Ave</p>
     <p class="max-w-sm uppercase text-xs">Venice, CA 90291</p>
     <p>e. duke@duketalent.agency</p>
     <p>p. 310 861 3300</p>
